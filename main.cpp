6f611a25d760bc0b8d451315f8924d28010eab6b
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2023-11-13 13:07:47 +0900

    コードを圧縮

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <Windows.h>
 #include <functional>
 
-typedef void (*pFunc)(int);
+typedef bool (*pFunc)(int);
 
 // 丁半を返す
-void CheckGambling(int num) {
+bool CheckGambling(int num) {
 	std::function<bool(int)> check = [](int i) {return rand() % 2 == i; };
 
 	std::function<void(int)> SetTimeOut = [](int time) {
@@ -19,18 +19,20 @@ void CheckGambling(int num) {
 
 	if (check(num)) {
 		printf("正解！さすがです！！！\n\n");
+		return true;
 	}
 	else {
 		printf("不正解！残念...\n\n");
+		return false;
 	}
 
 }
 
 void DiceGame(pFunc p) {
-	printf("0 : 丁, 1 : 半\n->");
-
 	std::function<int()> scan = []() {int num = 0; scanf_s("%d", &num); return num; };
-	p(scan());
+	do {
+		printf("0 : 丁, 1 : 半\n->");
+	} while (!p(scan()));
 }
 
 int main() {
